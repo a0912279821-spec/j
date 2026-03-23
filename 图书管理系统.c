2157387548e9abd7ebfc906e51d53bd9c4bc4dd4
a0912279821-spec <a0912279821@gmail.com>
2026-03-23 08:46:23 +0800

    Add files via upload

---
+++ 图书管理系统.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+struct Book
+{
+    int id[30];
+    char name[30];
+    char author[30];
+    char publisher[30];
+    double price[20];
+    int total[1000];
+    int stock[1000];
+};
